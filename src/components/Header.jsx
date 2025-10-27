c46e20fe14012360bf22f4d6b7b90b4571504ded
Fatima Massou <153992769+fatimamassou@users.noreply.github.com>
2025-10-27 16:33:20 +0000

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { ReactTyped } from "react-typed";
+
+function Header() {
+    return(
+        <header className="header">
+            <div className="header-left">
+                <img src="/jway-col.png" alt="logo" className="logo" />
+                <h1 className="company-name">JWORK</h1>
+            </div>
+            <div className="header-right">
+                <h1 className="solutions">
+                <ReactTyped
+                    strings={["Nos Solutions", "Innovantes", "Adaptées à Vous"]}
+                    typeSpeed={80}
+                    backSpeed={50}
+                    backDelay={1500}
+                    showCursor={true}
+                    cursorChar="|"
+                    loop={true}
+                />
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
