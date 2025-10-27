994f4a55d8b4f29348cc988286baa05552818467
Fatima Massou <153992769+fatimamassou@users.noreply.github.com>
2025-10-27 16:30:19 +0000

    add details component

---
+++ src/components/Details.jsx
@@ -0,0 +1,32 @@
+import { useParams } from "react-router-dom";
+import cardData from "../data/cardData";
+
+function DetailsPage() {
+  const { id } = useParams();
+  const card = cardData.find((c) => c.id === parseInt(id));
+
+  if (!card) {
+    return <h2 className="text-center mt-10">Solution not found </h2>;
+  }
+
+  return (
+    <div className="bubbles-container">
+      <main className="details-content">
+        <div className="details-content-card">
+          {card.image && <img src={card.image} alt={card.title} />}
+          <div className="title-section">
+            <h1>{card.title}</h1>                 
+            <h2>{card.subtitle}</h2>
+          </div>
+          <p>{card.description}</p>
+          <div className="tags">
+            <p>{card.tags[0]}</p>
+            <p>{card.tags[1]}</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default DetailsPage;
