23163b5f662d0570de41db07b254269a7f154422
Fatima Massou <153992769+fatimamassou@users.noreply.github.com>
2025-10-29 17:08:36 +0000

    add back icon

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,21 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import cardData from "../data/cardData";
+import { IoReturnUpBackOutline } from "react-icons/io5";
 
 function DetailsPage() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const card = cardData.find((c) => c.id === parseInt(id));
 
   if (!card) {
-    return <h2 className="text-center mt-10">Solution not found </h2>;
+    return <h2 className="text-center mt-10">Solution not found</h2>;
   }
 
   return (
     <div className="bubbles-container">
       <main className="details-content">
         <div className="details-content-card">
+          <IoReturnUpBackOutline 
+            className="back-icon" 
+            onClick={() => navigate("/")} 
+          />  
           {card.image && <img src={card.image} alt={card.title} />}
           <div className="title-section">
-            <h1>{card.title}</h1>                 
+            <h1>{card.title}</h1>
             <h2>{card.subtitle}</h2>
           </div>
           <p>{card.description}</p>
@@ -23,6 +29,7 @@ function DetailsPage() {
             <p>{card.tags[0]}</p>
             <p>{card.tags[1]}</p>
           </div>
+
         </div>
       </main>
     </div>
