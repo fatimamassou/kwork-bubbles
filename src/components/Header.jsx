54abfdb75971d714e924a578db5bb2a5dca50de4
Fatima Massou <153992769+fatimamassou@users.noreply.github.com>
2025-10-31 15:16:23 +0000

    add search icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,38 @@
 import { ReactTyped } from "react-typed";
+import { FaSearchengin } from "react-icons/fa";
 
-function Header() {
-    return(
-        <header className="header">
-            <div className="header-left">
-                <img src="/jway-col.png" alt="logo" className="logo" />
-                <h1 className="company-name">J<span className="company-name-highlight">work</span></h1>
-            </div>
-            <div className="header-right">
-                <h1 className="solutions">
-                <ReactTyped
-                    strings={["Nos Solutions", "Innovantes", "Adaptées à Vous"]}
-                    typeSpeed={80}
-                    backSpeed={50}
-                    backDelay={1500}
-                    showCursor={true}
-                    cursorChar="|"
-                    loop={true}
-                />
-                </h1>
-            </div>
-        </header>
-    )
+function Header({ onSearchClick }) {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <img src="/jway-col.png" alt="logo" className="logo" />
+        <h1 className="company-name">
+          J<span className="company-name-highlight">work</span>
+        </h1>
+      </div>
+
+      <div className="header-right">
+        <h1 className="solutions">
+          <ReactTyped
+            strings={["< Nos Solutions", "< Innovantes", "< Adaptées à Vous"]}
+            typeSpeed={80}
+            backSpeed={50}
+            backDelay={1500}
+            showCursor={true}
+            cursorChar=" />"
+            loop={true}
+          />
+        </h1>
+      </div>
+      <div className="search">
+        <FaSearchengin
+            className="search-icon"
+            onClick={onSearchClick}
+            title="Search"
+        />
+      </div>  
+    </header>       
+  );
 }
 
 export default Header;
